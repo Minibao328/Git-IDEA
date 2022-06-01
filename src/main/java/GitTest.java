4021388067c5bf7bb6204327359d05b06a6d879c
cherry <jyyr23@foxmail.com>
2022-06-01 15:37:17 +0800

    master commit2

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("hello2,Git");
         System.out.println("hot-fix,Git");
+        System.out.println("master,Git");
     }
 }
